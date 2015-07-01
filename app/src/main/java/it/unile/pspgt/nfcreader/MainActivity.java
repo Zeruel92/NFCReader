e3620356b304887e36652c6e5fcd0079980a80f9
pspgt <gabry.t92@gmail.com>
2015-07-01 16:09:34 +0200

    commentato ndef

--- app/src/main/java/it/unile/pspgt/nfcreader/MainActivity.java
+++ app/src/main/java/it/unile/pspgt/nfcreader/MainActivity.java
@@ -3,21 +3,13 @@ package it.unile.pspgt.nfcreader;
 import android.app.Activity;
 import android.app.PendingIntent;
 import android.content.Intent;
-import android.nfc.FormatException;
-import android.nfc.NdefMessage;
-import android.nfc.NdefRecord;
 import android.nfc.NfcAdapter;
 import android.nfc.Tag;
-import android.nfc.tech.Ndef;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.TextView;
-import android.widget.Toast;
-
-import java.io.IOException;
-import java.io.UnsupportedEncodingException;
 
 
 public class MainActivity extends Activity {
@@ -55,36 +47,23 @@ public class MainActivity extends Activity {
     @Override
     public void onResume(){
         super.onResume();
-        PendingIntent pending=PendingIntent.getActivity(this,0,new Intent(this,getClass()).addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP),0);
+        PendingIntent pending = PendingIntent.getActivity(this, 0, new Intent(this, getClass()).addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP), 0);
         adapter.enableForegroundDispatch(this,pending,null,null);
     }
     public void onNewIntent(Intent i){
         String action=i.getAction();
-        if (action.equals(adapter.ACTION_TAG_DISCOVERED)){
+        if (action.equals(NfcAdapter.ACTION_TAG_DISCOVERED)) {
            Log.i("TAG", "Tagletto");
             Bundle bundle=i.getExtras();
             Tag tag=bundle.getParcelable(NfcAdapter.EXTRA_TAG);
-            Ndef ntag= Ndef.get(tag);
-            try {
-                ntag.connect();
+
+            //  Ndef ntag= Ndef.get(tag)
+               /* ntag.connect();
                 NdefMessage message = ntag.getNdefMessage();
                 ntag.close();
-                NdefRecord[] nrecord=message.getRecords();
+                NdefRecord[] nrecord=message.getRecords();*/
                 //byte[] id=nrecord[0].getId();
-                text.setText(readText(nrecord[0]));
-            }catch(IOException e){
-                Log.e("Err","Errore di lettura");
-            } catch (FormatException e) {
-                e.printStackTrace();
-            }
+            text.setText(tag.getId().toString());
         }
-    }
-    private String readText(NdefRecord record) throws UnsupportedEncodingException {
-        byte[] payload = record.getPayload();
-        String textEncoding = ((payload[0] & 128) == 0) ? "UTF-8" : "UTF-16";
-        int languageCodeLength = payload[0] & 0063;
-        // String languageCode = new String(payload, 1, languageCodeLength, "US-ASCII");
-
-        return new String(payload, languageCodeLength + 1, payload.length - languageCodeLength - 1, textEncoding);
     }
 }
